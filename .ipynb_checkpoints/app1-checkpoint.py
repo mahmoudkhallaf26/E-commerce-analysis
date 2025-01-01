0fbfcf5f83bc67b95bce93462b5450c76509011e
mahmoud khallaf <mahmoudkhallaf26@gmail.com>
2025-01-01 14:50:05 +0200

    update

--- .ipynb_checkpoints/app1-checkpoint.py
+++ .ipynb_checkpoints/app1-checkpoint.py
@@ -21,49 +21,45 @@ def age(x):
 df["Age group"] = df["Age"].apply(age)        
 def page1():
     tab1,tab2,tab3 = st.tabs(['Category',"Item Purchased","user"])
-    season = st.sidebar.multiselect("Select Seasons",df["Season"].unique(),default=df["Season"].unique())
-    gender = st.sidebar.multiselect("Select gender",df["Gender"].unique(),default=df["Gender"].unique())
-    if not season or not gender:
-        st.write("⚠️ Please select at least one Season and Gender to display the data.")
-    else:
+    with tab1:
+        col1,col2 = st.columns(2)
+        season = st.sidebar.multiselect("Select Seasons",df["Season"].unique(),default=df["Season"].unique())
+        gender = st.sidebar.multiselect("Select gender",df["Gender"].unique(),default=df["Gender"].unique())
         mask = df["Season"].isin(season)
         mask1 = df["Gender"].isin(gender)
         df_filter = df[mask&mask1]
-        with tab1:
-            col1,col2 = st.columns(2)
-    
-            with col1:
-                st.plotly_chart(px.pie(data_frame=df_filter,names="Category",facet_col="Gender",hole=0.4,title="Product purchase percentage for each category by gender"))
-                st.plotly_chart(px.histogram(data_frame=df_filter,x="Category",color="Age group",barmode="group",title="Product purchase rate for each category by age group"))
-            with col2:    
-                st.plotly_chart(px.histogram(data_frame=df_filter,x="Category",y="total_Profit",color="Season",barmode="group",title="Total profit distribution by category and season"))
-                st.plotly_chart(px.histogram(data_frame=df_filter,x="Category",color="Discount Applied",barmode="group",title="Distribution of Categories by Discount Applied"))
-        with tab2:
-            col1,col2,col3 = st.columns(3)
-            with col1:
-                st.markdown("<p>TOP 10 Item have high rate</p>", unsafe_allow_html=True)
-                st.dataframe(pd.pivot_table(data=df_filter,index="Item Purchased",values="Review Rating").sort_values(by="Review Rating",ascending=False).head(10))
-                
-            with col2:
-                st.markdown("<p>TOP 10 Item have high Profit</p>", unsafe_allow_html=True)
-                st.dataframe(pd.pivot_table(data=df_filter,index="Item Purchased",values="total_Profit").sort_values(by="total_Profit",ascending=False).head(10))
-                
-            with col3:
-                st.plotly_chart(px.histogram(data_frame=df_filter["Item Purchased"].value_counts().sort_values(ascending=False).to_frame().reset_index().head(10),x="Item Purchased",y="count",title="TOP 10 Best Selling Products"))
+        with col1:
+            st.plotly_chart(px.pie(data_frame=df_filter,names="Category",facet_col="Gender",hole=0.4,title="Product purchase percentage for each category by gender"))
+            st.plotly_chart(px.histogram(data_frame=df_filter,x="Category",color="Age group",barmode="group",title="Product purchase rate for each category by age group"))
+        with col2:    
+            st.plotly_chart(px.histogram(data_frame=df_filter,x="Category",y="total_Profit",color="Season",barmode="group",title="Total profit distribution by category and season"))
+            st.plotly_chart(px.histogram(data_frame=df_filter,x="Category",color="Discount Applied",barmode="group",title="Distribution of Categories by Discount Applied"))
+    with tab2:
+        col1,col2,col3 = st.columns(3)
+        with col1:
+            st.markdown("<p>TOP 10 Item have high rate</p>", unsafe_allow_html=True)
+            st.dataframe(pd.pivot_table(data=df_filter,index="Item Purchased",values="Review Rating").sort_values(by="Review Rating",ascending=False).head(10))
+            
+        with col2:
+            st.markdown("<p>TOP 10 Item have high Profit</p>", unsafe_allow_html=True)
+            st.dataframe(pd.pivot_table(data=df_filter,index="Item Purchased",values="total_Profit").sort_values(by="total_Profit",ascending=False).head(10))
+            
+        with col3:
+            st.plotly_chart(px.histogram(data_frame=df_filter["Item Purchased"].value_counts().sort_values(ascending=False).to_frame().reset_index().head(10),x="Item Purchased",y="count",title="TOP 10 Best Selling Products"))
             
         
-            st.plotly_chart(px.treemap(data_frame=df_filter,path=["Item Purchased","Age group"],color_continuous_scale='Viridis',title="Map showing the most purchased product for each Age group"))
+        st.plotly_chart(px.treemap(data_frame=df_filter,path=["Item Purchased","Age group"],color_continuous_scale='Viridis',title="Map showing the most purchased product for each Age group"))
             
-        with tab3:
-            col1,col2 = st.columns(2)
-            with col1:
-                st.markdown("<p>Top 10 Location make order and Shipping Type use</p>",unsafe_allow_html=True)
-                st.dataframe(pd.pivot_table(data=df_filter,columns="Shipping Type",index="Location",values="Customer ID",aggfunc="count",margins=True).sort_values(by="All",ascending=False)[1:].head(10))
-                st.plotly_chart( px.pie(data_frame=df,names="Payment Method",title="Percentage of each payment method"))
-            with col2:
-                st.plotly_chart(px.histogram(data_frame=df,x="Frequency of Purchases",color="Subscription Status",barmode="group",title="Distribution of purchase frequency by subscription status"))
-                st.plotly_chart(px.histogram(data_frame=df_filter,x="Shipping Type",color="Subscription Status" , barmode="group",title="Shipping companies users from subscribers and non-subscribers"))
-                st.plotly_chart(px.treemap(data_frame=df,path=["Age group","Subscription Status"],title="Tree map showing subscription status within age groups"))
+    with tab3:
+        col1,col2 = st.columns(2)
+        with col1:
+            st.markdown("<p>Top 10 Location make order and Shipping Type use</p>",unsafe_allow_html=True)
+            st.dataframe(pd.pivot_table(data=df_filter,columns="Shipping Type",index="Location",values="Customer ID",aggfunc="count",margins=True).sort_values(by="All",ascending=False)[1:].head(10))
+            st.plotly_chart( px.pie(data_frame=df,names="Payment Method",title="Percentage of each payment method"))
+        with col2:
+            st.plotly_chart(px.histogram(data_frame=df,x="Frequency of Purchases",color="Subscription Status",barmode="group",title="Distribution of purchase frequency by subscription status"))
+            st.plotly_chart(px.histogram(data_frame=df_filter,x="Shipping Type",color="Subscription Status" , barmode="group",title="Shipping companies users from subscribers and non-subscribers"))
+            st.plotly_chart(px.treemap(data_frame=df,path=["Age group","Subscription Status"],title="Tree map showing subscription status within age groups"))
 def page2():
     st.markdown("<h2>summary</h2>",unsafe_allow_html=True)
     col1,col2,col3 = st.columns(3)
